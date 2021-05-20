ee3a04a37be174fa3bb89cd04b948f10b400f193
Jay-6957 <jkp6957@gmail.com>
2021-05-20 13:30:37 +0530

    UPDATED SETTING Commit

--- SWC/settings.py
+++ SWC/settings.py
@@ -18,14 +18,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+SECRET_KEY=os.environ.get("SECRET_KEY")
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-06%^(=^v5@5x8f2lu$58-bfvjn1ibs*)ksls97mqxe9ze@znnv'
+# SECRET_KEY = 'django-insecure-06%^(=^v5@5x8f2lu$58-bfvjn1ibs*)ksls97mqxe9ze@znnv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['swctestapp.herokuapp.com']
 
 
 # Application definition
